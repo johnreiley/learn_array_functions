76a7dc26dd543cb50aa5c3a1b2c8137c7b95a4f0
John Reiley <rei15006@byui.edu>
2019-05-03 12:01:21 -0600

    Adds changes to slice() example

--- 5_slice.js
+++ 5_slice.js
@@ -1,12 +1,18 @@
 /*
 What does it do?
-    - 
+    - Returns a copy of the array using the parameters that specify the
+    indexes of where the slice will occur ()
 Does it edit the current array?
     - 
 What does it return?
     - 
 */
-
+//------------------------------------------------------------------------
 // How can I use it?
 
+let array = ['e1', 'e2', 'e3', 'e4', 'e5'];
+
+let newArray = array.slice(0, 2);
+
+//------------------------------------------------------------------------
 // Real World Example!
