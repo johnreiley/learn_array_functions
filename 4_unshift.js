20bcf381ba8dfa1619702a10553175ed64016f3f
John Reiley <rei15006@byui.edu>
2019-05-03 11:39:59 -0600

    Adds completed unshift() example

--- 4_unshift.js
+++ 4_unshift.js
@@ -1,12 +1,30 @@
 /*
 What does it do?
-    - 
+    - Adds one or more elements to the beginning of an array
 Does it edit the current array?
-    - 
+    - Yes
 What does it return?
-    - 
+    - The new length of the array
 */
-
+//------------------------------------------------------------------------
 // How can I use it?
-
+let array = ['e1', 'e2', 'e3'];
+console.log("The array: " + array);
+// e1, e2, e3
+console.log("New length of array: " + array.unshift('e0'));
+// 4
+console.log("The array: " + array);
+// e0, e1, e2, e3
+//------------------------------------------------------------------------
 // Real World Example!
+let shoppingList = {
+    list: ['chips', 'salsa', 'sour cream', 'oj'],
+    addNewItem: function (newItem) {
+        return this.list.unshift(newItem);
+    }
+};
+
+console.log("Shopping List: " + shoppingList.list)
+shoppingList.addNewItem("eggs");
+console.log("Shopping List: " + shoppingList.list);
+// eggs, chips, salsa, sour cream, oj
