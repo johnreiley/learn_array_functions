0fd526382796479f094b3ddc4d933482a79bd1bc
John Reiley <rei15006@byui.edu>
2019-05-03 15:56:17 -0600

    Adds the completed sort() example

--- 6_sort.js
+++ 6_sort.js
@@ -1,12 +1,51 @@
 /*
 What does it do?
-    - 
+    - Sorts the array by UTF-16 Unicode order when call with no parameters
+    - Accepts a function as a parameter that can be used to custom sort
 Does it edit the current array?
-    - 
+    - Yes
 What does it return?
-    - 
+    - The sorted array
+How can I use it? -- If you have a class list of students and want to 
+sort them alphabetically.
 */
-
-// How can I use it?
-
+//------------------------------------------------------------------------
 // Real World Example!
+let students = [{
+        firstName: "Bob",
+        lastName: "Marley",
+        Id: 13579246
+    },
+    {
+        firstName: "Zenora",
+        lastName: "Waggle",
+        Id: 48572957
+    },
+    {
+        firstName: "Henry",
+        lastName: "Ford",
+        Id: 13579246
+    },
+    {
+        firstName: "Wane",
+        lastName: "Durgery",
+        Id: 13579246
+    },
+];
+
+console.log("Class List Before Sort:")
+for (let student of students) {
+    console.log(student.lastName);
+}
+
+let sortedStudents = students.sort(function (a, b) {
+    if (a.lastName == b.lastName) {
+        return 0;
+    }
+    return (a.lastName < b.lastName) ? -1 : 1;
+});
+
+console.log("Class List After Sort:")
+for (let student of students) {
+    console.log(student.lastName);
+}
