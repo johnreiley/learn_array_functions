a13c723bbf6edb32465ddf55d20f26e9fea2f4f9
John Reiley <supermormonbrother@gmail.com>
2019-05-08 16:47:23 -0600

    Adds the completed forEach() example

--- 8_forEach.js
+++ 8_forEach.js
@@ -1,12 +1,20 @@
 /*
 What does it do?
-    - 
+    - forEach iterates through the given array, running the function
+    passed as a parameter for each item in the array
 Does it edit the current array?
-    - 
+    - No
 What does it return?
-    - 
+    - undefined
+How can I use it?
+    - If I have an array of data that I want to read and copy to a new array
 */
-
-// How can I use it?
-
 // Real World Example!
+let dataArray = ["!!!!", "@@@@", "####", "$$$$", "%%%%"];
+let arrayCopy = [];
+
+dataArray.forEach(function(item, index) {
+    console.log("Item " + (index + 1) + ": " + item);
+    arrayCopy.push(item);
+});
+console.log("COPY: " + arrayCopy);
