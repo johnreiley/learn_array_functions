0b0d288ad89f9594486cce78883d9204d9d40d48
John Reiley <rei15006@byui.edu>
2019-05-09 16:01:48 -0600

    Adds the completed every()

--- 10_every.js
+++ 10_every.js
@@ -1,12 +1,44 @@
 /*
 What does it do?
-    - 
+    - Goes through the given array and checks each value
+    against the callback function parameter
 Does it edit the current array?
-    - 
+    - No
 What does it return?
-    - 
+    - True if every value returns true from the callback,
+    if not, False.
+How can I use it?
+    - You have a list of students and want to confirm 
+    whether or not they are all registered for classes 
+    in the upcoming semester.
 */
-
-// How can I use it?
-
+//------------------------------------------------------------------------
 // Real World Example!
+let students = [{
+        name: "Bobby Darin",
+        numRegisteredCredits: 12
+    },
+    {
+        name: "Henry Ford",
+        numRegisteredCredits: 7
+    },
+    {
+        name: "Josh McKinney",
+        numRegisteredCredits: 0
+    },
+    {
+        name: "Carey Grant",
+        numRegisteredCredits: 14
+    },
+    {
+        name: "Peter Parker",
+        numRegisteredCredits: 200
+    },
+];
+
+let isRegistered = students.every(function (student) {
+    return student.numRegisteredCredits > 0;
+})
+
+if (isRegistered) console.log("All students are registered");
+else console.log("Not all students are registered");
