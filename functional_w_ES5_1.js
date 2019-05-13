d5a72cec14f41cb7ea6fbb6ec10c18bdf8b75a30
John Reiley <rei15006@byui.edu>
2019-05-13 17:07:51 -0600

    Updates progress on Functional ES5 Ex 1

---
+++ functional_w_ES5_1.js
@@ -0,0 +1,30 @@
+let carDetailsArray = [{
+        key: "year",
+        value: "2016"
+    },
+    {
+        key: "make",
+        value: "Porsche"
+    },
+    {
+        key: "model",
+        value: "911 R"
+    },
+    {
+        key: "color",
+        value: "white"
+    },
+    {
+        key: "msrp",
+        value: "$184,900"
+    }
+];
+
+function convertToCarObject(carDetailsArray) {
+    return carDetailsArray.filter(function (item) {
+            return item.key != "msrp";
+        })
+        .reduce(function (item) {
+
+        }, {})
+}
